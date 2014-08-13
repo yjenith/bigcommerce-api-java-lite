c5e1d53be3bdc144c4e774fd18d60183c4ab0139
Y. Jenith Michael Raj <yjenith@users.noreply.github.com>
2014-08-13 18:34:52 +0530

    Filtering option is provided.

--- src/main/java/com/bigcommerce/api/resources/Resource.java
+++ src/main/java/com/bigcommerce/api/resources/Resource.java
@@ -2,9 +2,11 @@ package com.bigcommerce.api.resources;
 
 import java.util.List;
 
+import com.bigcommerce.api.Filter;
 import com.bigcommerce.api.Response;
 
 public interface Resource {
 	public List<? extends Response> listAll();
+	public List<? extends Response> listAll(Filter filter);
 	public Response getOne(Integer Id);
 }
