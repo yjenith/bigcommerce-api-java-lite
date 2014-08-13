5db0f4596e141d8115cd2b8d2ffd7827b8f9cb21
Y. Jenith Michael Raj <yjenith@users.noreply.github.com>
2014-08-13 18:38:24 +0530

    Filter parameters added
    Filter parameters are added to the URL query string to select specific resources

--- src/test/java/com/bigcommerce/api/OrderTest.java
+++ src/test/java/com/bigcommerce/api/OrderTest.java
@@ -11,25 +11,64 @@ public class OrderTest {
         	String use	= "admin";
         	String apiKey   = "akjfalksjflksjflaskdjflasdk";
 
-		Store storeApi = new Store(storeUrl, user, apiKey);
-		Orders orders = storeApi.getOrders();
+		// Get Store API connection
+		Store api = new Store(storeUrl, user, apiKey);
 
-		Collection<Order> allOrders = orders.listAll();
+		listOrdersWithNoFilter(api);
+		listOrdersWithFilter(api);
+	}
+
+	/**
+	 * Test Orders with No Filter
+	 * @param api
+	 */
+	public static void listOrdersWithNoFilter(Store api) {
+		System.out.println("Order Test without filter-----");
+		Collection<Order> orders = api.getOrders().listAll();
 
 		for (Order order : orders) {
 			System.out.println("Customer ID:" + order.getCustomerId());
 			System.out.println("Order ID:" + order.getId());
 			System.out.println("Order Status:" + order.getStatus());
-			
+
 			// List all products associated with an order
 			for (OrderProduct orderProduct : order.getOrderProducts()) {
 				System.out.println("Product ID:" + orderProduct.getId());
 				System.out.println("Product Name:" + orderProduct.getName());
-				System.out.println("Product Quantity:" + orderProduct.getQuantity());
+				System.out.println("Product Quantity:"
+						+ orderProduct.getQuantity());
 			}
-			
+
 			System.out.println("---------------------");
 		}
 	}
+
+	/**
+	 * Test Orders with Filter
+	 * @param api
+	 */
+	public static void listOrdersWithFilter(Store api) {
+		System.out.println("Order Test with min_id filter-----");
+		
+		// Filter parameters
+		Filter filter = Filter.create();
+		filter.add("min_id", "107");
+		Collection<Order> orders = api.getOrders().listAll(filter);
+
+		for (Order order : orders) {
+			System.out.println("Customer ID:" + order.getCustomerId());
+			System.out.println("Order ID:" + order.getId());
+			System.out.println("Order Status:" + order.getStatus());
+
+			// List all products associated with an order
+			for (OrderProduct orderProduct : order.getOrderProducts()) {
+				System.out.println("Product ID:" + orderProduct.getId());
+				System.out.println("Product Name:" + orderProduct.getName());
+				System.out.println("Product Quantity:"
+						+ orderProduct.getQuantity());
+			}
+
+			System.out.println("---------------------");
+		}
 
 }
