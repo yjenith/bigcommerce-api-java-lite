9826d1c1abdc668ae8310908dd1146dee4145b5b
Y. Jenith Michael Raj <yjenith@users.noreply.github.com>
2014-08-29 07:41:32 +0530

    Update (PUT) a resource API call is added.

--- src/main/java/com/bigcommerce/api/resources/Resource.java
+++ src/main/java/com/bigcommerce/api/resources/Resource.java
@@ -3,10 +3,12 @@ package com.bigcommerce.api.resources;
 import java.util.List;
 
 import com.bigcommerce.api.Filter;
+import com.bigcommerce.api.Form;
 import com.bigcommerce.api.Response;
 
 public interface Resource {
 	public List<? extends Response> listAll();
 	public List<? extends Response> listAll(Filter filter);
 	public Response getOne(Integer Id);
+	public Boolean update(Integer Id, Form data);
 }
