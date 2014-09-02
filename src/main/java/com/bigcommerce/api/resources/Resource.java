1fe40633db2b863cc91f135a454ac624b7ec06c9
Y. Jenith Michael Raj <yjenith@users.noreply.github.com>
2014-09-02 12:13:39 +0530

    Update Resource.java

--- src/main/java/com/bigcommerce/api/resources/Resource.java
+++ src/main/java/com/bigcommerce/api/resources/Resource.java
@@ -7,8 +7,11 @@ import com.bigcommerce.api.Form;
 import com.bigcommerce.api.Response;
 
 public interface Resource {
+	public void setId(Integer id);
+	public Boolean create(Form data);
+	public Boolean update(Form data);
 	public List<? extends Response> listAll();
 	public List<? extends Response> listAll(Filter filter);
-	public Response getOne(Integer Id);
-	public Boolean update(Integer Id, Form data);
+	public Response get();
+	public Boolean delete();
 }
