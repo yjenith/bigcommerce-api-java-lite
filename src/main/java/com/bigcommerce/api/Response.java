f18cbf7a3f5e6c9294610bde54e142dcfc6ad2af
Y. Jenith Michael Raj <yjenith@users.noreply.github.com>
2014-09-02 12:20:25 +0530

    Update Response.java

--- src/main/java/com/bigcommerce/api/Response.java
+++ src/main/java/com/bigcommerce/api/Response.java
@@ -4,19 +4,30 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-class Resource {
+public abstract class Response {
 	Element document;
 
-	Resource(Element document) {
+	Response() {
+	}
+
+	Response(Element document) {
 		this.document = document;
 	}
 
-	protected String getScalarField(String name) {
+	public void setDocument(Element document) {
+		this.document = document;
+	}
+
+	public String getScalarField(String name) {
 		NodeList elements = document.getElementsByTagName(name);
 		Node field = elements.item(0);
 		if (field != null) {
 			return field.getTextContent();
 		}
 		return null;
+	}
+
+	public Element asXml() {
+		return document;
 	}
 }
